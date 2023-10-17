797ae320df19a95a6a81e9c56b47b7e8daef5cd9
Vitaliy Beyar <37002507+nightven@users.noreply.github.com>
2023-10-17 05:21:30 +0200

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,59 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { Phonebook } from './Phonebook/Phonebook';
+import { Contacts } from './Contacts/Contacts';
+import { Container, Label } from './App.styled';
+import { getContacts, getFilter } from 'redux/selectors';
+import { deleteContact } from 'redux/contactSlice';
+import { setFilter } from 'redux/filterSlice';
+
+
+export const App = () => {
+  
+  const dispatch = useDispatch();
+  const contacts = useSelector(getContacts);
+  const filter = useSelector(getFilter);
+
+  //! delete a contact from state
+  const onDeleteContact = id => {
+    dispatch(deleteContact(id));
+  };
+
+  //! set filter from input field
+  const onInputChangeFilter = e => {
+    dispatch(setFilter(e.target.value.trim()));
+  };
+
+  //! filtered contacts by name
+  const filteredContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+  
+
+  return (
+    <Container>
+      <Phonebook />
+      <h2>Contacts</h2>
+      {/* show search input if contacts isn't empty */}
+      {contacts.length === 0 ? (
+        <p>Empty</p>
+      ) : (
+        <div>
+          <Label>
+            <p>Search by contact</p>
+            <input
+              type="text"
+              placeholder="Enter search contact"
+              value={filter}
+              onChange={onInputChangeFilter}
+            />
+          </Label>
+          <Contacts
+         
+            contacts={filteredContacts}
+            onDeleteContact={onDeleteContact}
+          />
+        </div>
+      )}
+    </Container>
+  );
+};
